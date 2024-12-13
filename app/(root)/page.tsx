e8ad2a05c9b610962262d19bdc9d9e8fca95d539
Alexandre Cottin (Lx-Ctn) <design@lxctn.com>
2024-12-13 10:49:22 +0100

    set right siedebar UI on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSideBar from "@/components/RightSideBar";
 
 const Home = () => {
-	const loggedIN = { firstname: "Lex" };
+	const loggedIn = { firstName: "Lex", lastName: "CTN", email: "design@lxctn.com" };
 	return (
 		<section className="home">
 			<div className="home-content">
@@ -11,12 +12,18 @@ const Home = () => {
 					<HeaderBox
 						type="greeting"
 						title="Welcome"
-						user={loggedIN?.firstname || "Guest"}
+						user={loggedIn?.firstName || "Guest"}
 						subtext="Access and manage your account and transactions efficiently."
 					/>
 					<TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
 				</header>
+				RECENT TRANSACTIONS
 			</div>
+			<RightSideBar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 1435.45 }, { currentBalance: 1435.45 }]}
+			/>
 		</section>
 	);
 };
